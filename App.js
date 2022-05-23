670b29a77e3d3935ba1ec82440fdbdd282ff14e3
nowshad-netizen <lewsedmond@gmail.com>
2022-05-23 09:55:32 +0600

    text color preset

--- App.js
+++ App.js
@@ -1,15 +1,37 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { colors } from "./src/theme/color";
+import { spacing } from "./src/theme/spacing";
+import { useFonts } from 'expo-font';
+import Text from "./src/components/text";
 
 export default function App() {
+  const [loaded] = useFonts({
+    'Antonio-Regular': require('./assets/fonts/Antonio-Regular.ttf'),
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Regular': require('./assets/fonts/Spartan-Regular.ttf')
+  });
+
+  if (!loaded) {
+    return null;
+  };
   return (
-    <View
-      style={{
+    <>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: colors.warningText
+        }}
+      >
+        <Text preset="h1">Universal React with Expo</Text>
+      </View>
+      <View style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+        backgroundColor: colors.warningBackground
+      }}>
+        <Text style={{ fontSize: 20, color: colors.black, textAlign: 'center', padding: spacing[2] }}>Text</Text>
+      </View>
+    </>
   );
 }
---
+++ src/theme/color.js
@@ -0,0 +1,11 @@
+export const colors = {
+    black: '#078724',
+    white: '#fff',
+    tabIconDefault: '#959292',
+    tabBar: '#fefefe',
+    errorBackground: 'red',
+    errorText: '#fff',
+    warningBackground: '#EAEB5E',
+    warningText: '#666804',
+    noticeText: '#fff'
+}
